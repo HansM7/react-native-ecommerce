62748710efb04e9cf7bc11a47c683465e935e4fc
HansM7 <melchorhans5@gmail.com>
2024-02-18 19:30:08 -0500

    Add details card

--- components/screens/detail/index.jsx
+++ components/screens/detail/index.jsx
@@ -1,5 +1,4 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import FooterComponent from "../../elements/footer";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { useState } from "react";
 import { initialDataProducts } from "../../data/data-ecommerce";
@@ -23,8 +22,88 @@ function ProductSreen() {
           }}
           style={styles.image}
         />
+        <View style={styles.container_detail}>
+          <View style={styles.first_content}>
+            <View style={styles.first_content_left}>
+              <Text style={styles.title}>Title product</Text>
+              <Text style={styles.description}>Description product</Text>
+              <View style={styles.content_calification}>
+                <View style={styles.content_stars}>
+                  <Text>⭐</Text>
+                  <Text>⭐</Text>
+                  <Text>⭐</Text>
+                  <Text>⭐</Text>
+                  <Text>⭐</Text>
+                </View>
+                <Text> {"(300)"} Reviews</Text>
+              </View>
+            </View>
+            <View style={styles.first_content_right}>
+              <View style={styles.content_ammount}>
+                <TouchableOpacity>
+                  <Text style={styles.text_ammount}>-</Text>
+                </TouchableOpacity>
+                <Text style={styles.text_ammount}>5</Text>
+                <TouchableOpacity>
+                  <Text style={styles.text_ammount}>+</Text>
+                </TouchableOpacity>
+              </View>
+              <Text style={styles.text_stock}>Aviable in stock</Text>
+            </View>
+          </View>
+          {/* Second -------------------------------------- */}
+          <View style={styles.second_content}>
+            <View style={styles.size_content}>
+              <Text style={styles.text_size}>Size</Text>
+              <View style={styles.option_size}>
+                <View style={styles.item_size}>
+                  <Text style={styles.text_item_size}>S</Text>
+                </View>
+                <View style={styles.item_size}>
+                  <Text style={styles.text_item_size}>M</Text>
+                </View>
+                <View style={styles.item_size}>
+                  <Text style={styles.text_item_size}>L</Text>
+                </View>
+                <View style={styles.item_size}>
+                  <Text style={styles.text_item_size}>XL</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.color_content}>
+              <View style={styles.color_1}></View>
+              <View style={styles.color_2}></View>
+              <View style={styles.color_3}></View>
+              <View style={styles.color_4}></View>
+            </View>
+          </View>
+          {/* third -------------------------------------- */}
+          <View style={styles.third_content}>
+            <Text style={styles.title_description}>Description</Text>
+            <Text style={styles.content_description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque est
+              aperiam assumenda magnam et cumque illo doloribus. Assumenda ae
+              quia rerum ex odit.
+            </Text>
+            <View style={styles.final_cotent}>
+              <View style={styles.final_price}>
+                <Text style={styles.price_title}>Total price</Text>
+                <Text style={styles.price_ammount}>$2000.00</Text>
+              </View>
+              <View>
+                <TouchableOpacity style={styles.button_add}>
+                  <Image
+                    source={require("../../../assets/icons/profile_black.png")}
+                    // style={styles.icon}
+                  />
+                  <Text style={styles.text_button_add}>Add to card</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
-      <FooterComponent screen={screen}></FooterComponent>
+      {/* <FooterComponent screen={screen}></FooterComponent> */}
     </View>
   );
 }
@@ -35,16 +114,196 @@ const styles = StyleSheet.create({
   },
   container_product: {
     position: "relative",
-    paddingHorizontal: 20,
+    // paddingHorizontal: 20,
     width: "100%",
-    marginBottom: 50,
+    // marginBottom: 50,
     flexDirection: "column",
     minHeight: "100%",
     backgroundColor: "red",
   },
   image: {
     width: "100%",
-    height: "400",
+    height: 500,
+  },
+  container_detail: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    backgroundColor: "white",
+    paddingVertical: 40,
+    borderColor: "#f6f6f6",
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+
+    elevation: 24,
+    paddingHorizontal: 40,
+    // height: "100%",
+  },
+
+  first_content: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  first_content_left: {
+    flexDirection: "column",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  description: {
+    color: "gray",
+    fontSize: 16,
+  },
+  first_content_right: {
+    flexDirection: "column",
+    gap: 2,
+    alignItems: "flex-end",
+  },
+  content_calification: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  content_ammount: {
+    flexDirection: "row",
+    backgroundColor: "#e7e7e7",
+    borderRadius: 20,
+    overflow: "hidden",
+    justifyContent: "space-around",
+    padding: 8,
+    width: 100,
+  },
+  text_ammount: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  text_stock: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  content_stars: {
+    flexDirection: "row",
+    gap: 4,
+  },
+  // ----------------------------------------
+
+  second_content: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  size_content: {
+    flexDirection: "column",
+    gap: 4,
+  },
+  text_size: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  option_size: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  item_size: {
+    borderRadius: 20,
+    backgroundColor: "black",
+    width: 40,
+    height: 40,
+    justifyContent: "center",
+  },
+  text_item_size: {
+    textAlign: "center",
+    color: "white",
+  },
+  color_content: {
+    flexDirection: "column",
+    gap: 4,
+    borderRadius: 10,
+    elevation: 10,
+    backgroundColor: "white",
+    padding: 8,
+    alignItems: "center",
+  },
+
+  color_1: {
+    borderRadius: 20,
+    width: 30,
+    height: 30,
+    backgroundColor: "yellow",
+  },
+  color_2: {
+    borderRadius: 20,
+
+    width: 25,
+    height: 25,
+    backgroundColor: "blue",
+  },
+  color_3: {
+    borderRadius: 20,
+
+    width: 25,
+    height: 25,
+    backgroundColor: "violet",
+  },
+  color_4: {
+    borderRadius: 20,
+
+    width: 25,
+    height: 25,
+    backgroundColor: "black",
+  },
+
+  // ----------------------------------------
+
+  third_content: {
+    marginTop: 20,
+    flexDirection: "column",
+    gap: 12,
+  },
+  title_description: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  content_description: {
+    color: "gray",
+  },
+  final_cotent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 8,
+  },
+  final_price: {
+    flexDirection: "column",
+  },
+  price_title: {
+    fontSize: 14,
+    color: "gray",
+  },
+  price_ammount: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  button_add: {
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 20,
+    backgroundColor: "black",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text_button_add: {
+    color: "white",
+    fontSize: 18,
   },
 });
 
